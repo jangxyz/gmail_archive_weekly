89b67cf32ac63883b94d1445183384e2ba0a27a5
Kim, Jang-hwan <janghwan@gmail.com>
2012-07-02 00:30:04 +0900

    add more pretty print

--- archive_weekly.py
+++ archive_weekly.py
@@ -6,6 +6,28 @@ import getpass
 import datetime
 from email.header import decode_header
 
+def print_mail_info(_id, header):
+    import email
+    import re
+    email.header.ecre = re.compile(r'''
+      =\?                   # literal =?
+      (?P<charset>[^?]*?)   # non-greedy up to the next ? is the charset
+      \?                    # literal ?
+      (?P<encoding>[qb])    # either a "q" or a "b", case insensitive
+      \?                    # literal ?
+      (?P<encoded>.*?)      # non-greedy up to the next ?= is the encoded string
+      \?=                   # literal ?=
+    ''', re.VERBOSE | re.IGNORECASE | re.MULTILINE) # easier parsing
+    def decode(header_line):
+        decoded = []
+        for s, encoding in decode_header(header_line):
+            if not encoding:
+                encoding = 'utf8'
+            decoded.append(s.decode(encoding))
+        return ''.join(decoded)
+    header_dict = dict(map(lambda x: x.strip(), decode(line).split(':', 1)) for line in header.strip().splitlines())
+    print '[%(Date)s] %(From)s  =>  "%(Subject)s"' % header_dict
+
 
 def main():
     '''
@@ -24,6 +46,7 @@ def main():
     server = imapclient.IMAPClient('imap.gmail.com', ssl=True)
     server.login(username, password)
     del password
+    print 'logged in with', username
 
     # list
     folders = server.xlist_folders() 
@@ -39,47 +62,7 @@ def main():
     print len(labled_msgs), 'labled messages'
 
     # print
-    def print_mail_info(_id, header):
-        import re
-        ecre = re.compile(r'''
-          =\?                   # literal =?
-          (?P<charset>[^?]*?)   # non-greedy up to the next ? is the charset
-          \?                    # literal ?
-          (?P<encoding>[qb])    # either a "q" or a "b", case insensitive
-          \?                    # literal ?
-          (?P<encoded>.*?)      # non-greedy up to the next ?= is the encoded string
-          \?=                   # literal ?=
-        ''', re.VERBOSE | re.IGNORECASE | re.MULTILINE)
-        import email
-        email.header.ecre = ecre
-        def decode(header_line):
-            #decoded = [s.decode(encoding if encoding else 'utf8') for (s,encoding) in decode_header(header_line.strip())]
-            decoded = []
-            for s, encoding in decode_header(header_line):
-                if not encoding:
-                    encoding = 'utf8'
-                decoded.append(s.decode(encoding))
-            return ''.join(decoded)
-        #subject = mail['BODY[HEADER.FIELDS (SUBJECT)]'].replace("\r", "").replace("\n", "")[len('Subject: ')-1:]
-        #subject = [(s,encoding) for (s,encoding) in decode_header(subject)]
-        #subject = [s.decode(encoding if encoding else 'utf8') for (s,encoding) in decode_header(subject)][0]
-        #print _id, data['SUBJECT'], data['FROM'], data['DATE']
-        #
-        #data = {}
-        #for field_name in ['SUBJECT', 'FROM', 'DATE']:
-        #    data[field_name] = mail['BODY[HEADER.FIELDS (%s)]' % field_name]
-        #    data[field_name] = data[field_name].replace('\r', '').replace('\n', '')
-        #    # strip prefix (ex: leading 'Subject: ')
-        #    data[field_name] = data[field_name][len(field_name)+2:]
-        #    # decode
-        #    data[field_name] = decode(data[field_name])
-        #print "[%(DATE)s] %(FROM)s: %(SUBJECT)s" % data
-        #
-        print " / ".join([decode(line) for line in header.strip().splitlines()])
-            
-
     if len(labled_msgs) <= 30:
-        #result = server.fetch(labled_msgs, ['BODY[HEADER.FIELDS (SUBJECT)]', 'BODY[HEADER.FIELDS (FROM)]', 'BODY[HEADER.FIELDS (DATE)]'])
         result = server.fetch(labled_msgs, ['BODY[HEADER.FIELDS (SUBJECT FROM DATE)]'])
         for _id, mail in result.iteritems():
             print_mail_info(_id, mail['BODY[HEADER.FIELDS (SUBJECT FROM DATE)]'])
